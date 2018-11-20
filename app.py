9fe10665c8bd616ffa0002fc154e29eef503728b
mendible <mendible@uw.edu>
2018-11-20 15:27:18 -0800

    adding plot callback

--- app.py
+++ app.py
@@ -182,6 +182,45 @@ def run_rpca(n_clicks, filename, slider_value):
         return 'RPCA run on {} with \u03bb = {:0.2f} and saved'.format(filename, lambda_value)
     return None
 
+
+# plotting stuff
+@app.callback(Output('full-data-plot', 'figure'),
+              [Input('plot-button', 'n_clicks')],
+              [State('crossfilter-yaxis-column', 'value'),
+               State('lambda-slider', 'value'),
+               State('upload-data', 'filename')])
+def update_graph_full(n_clicks, yaxis_column_name, slider_value, filename):
+    '''
+    This function updates the plot of the raw data.
+    '''
+    if n_clicks is not None:
+        lambda_value = 10**(slider_value-1)
+        filename, _ = os.path.splitext(filename) # new fix for filename
+        rpca_filename = '{}_lam{:0.2f}_rpca.mat'.format(filename, lambda_value)
+        if os.path.isfile(rpca_filename):
+            mat = io.loadmat(rpca_filename, mat_dtype=True)
+            color_limits = mat['color_limits']
+            fulldata = mat['fulldata']
+            plot_z = fulldata[:, :, 1]
+        else: plot_z = 1
+        return {
+            'data': [go.Heatmap(
+                z=plot_z,
+                colorbar={'x':-0.15}, # position of the colorbar
+                zmin=color_limits[0][0],
+                zmax=color_limits[0][1],
+            )],
+            'layout': go.Layout(
+                xaxis={'showticklabels':False, 'ticks':''},
+                yaxis={'showticklabels':False, 'ticks':''},
+                title='Full Data: ' + str(yaxis_column_name),
+                margin={'l':40, 'b':40, 't':40, 'r':40},
+                height=450,
+                #hovermode='closest'
+            )
+        }
+    return None
+
 EXTERNAL_CSS = [
     # Normalize the CSS
     "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css",
